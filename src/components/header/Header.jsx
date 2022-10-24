6165d73d2af9e5f1263ffff4b5bf39df03f4662d
Agrossio <abossio@gmail.com>
2022-10-24 17:39:42 -0300

    Header Buttons OK

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/Foto 3.png'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <header>
+            <div className="container header__container">
+                <h5>Hello I'm</h5>
+                <h1>Matias Abossio</h1>
+                <h5 className="text-light">Fullstack Developer</h5>
+                <CTA />
+
+                <div className="me">
+                    <img src={ME} alt="Matias" />
+                </div>
+
+                <a href="#contact" className='scroll_down'>Scroll Down</a>
+
+            </div>
+        </header>
     )
 }
 
