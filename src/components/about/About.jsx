3806553575c31a9a80bb7c80cb6ba2dfa715a3b2
Agrossio <abossio@gmail.com>
2022-10-25 12:44:59 -0300

    Styling

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/profileImage/Foto 3.png'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
     return (
 
-        <section id='about'>About</section>
+        <section id='about'>
+            <h5>Get To Know</h5>
+            <h2>About Me</h2>
+            <div className='container about__container'>
+                <div className='about__me'>
+                    <div className='about__me-image'>
+                        <img src={ME} alt="About Image" />
+                    </div>
+                </div>
+                <div className='about__content'>
+                    <div className='about__cards'>
+                        <article className='about__card'>
+                            <FaAward className='about__icon' />
+                            <h5>Experience</h5>
+                            <small>3+ Years Working</small>
+                        </article>
+                        <article className='about__card'>
+                            <FiUsers className='about__icon' />
+                            <h5>Clients</h5>
+                            <small>200+ Clients Worldwide</small>
+                        </article>
+                        <article className='about__card'>
+                            <VscFolderLibrary className='about__icon' />
+                            <h5>Projects</h5>
+                            <small>80+ Completed Projects</small>
+                        </article>
+                    </div>
+                    <p>My name is Matias Abossio, I'm a Jr Full Stack Developer from Argentina and also a 9 year Consuming Goods Industry experienced Engineer. I've graduated from Plataforma 5 Coding Bootcamp, adding +800 hours of coding applying Pair Programming and Agile Methodologies to my background.
+                    </p>
+
+                    <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+                </div>
+            </div>
+        </section>
     )
 }
 
