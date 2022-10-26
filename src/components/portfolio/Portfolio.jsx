77548af04783ad123591a50604a6827b1ba3a151
Agrossio <abossio@gmail.com>
2022-10-26 13:00:51 -0300

    portfolio 1.0

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './portfolio.css'
+import projectImg1 from '../../assets/projects/project1.png'
 
 const Portfolio = () => {
     return (
-        <section id='portfolio'>Portfolio</section>
+        <section id='portfolio'>
+            <h5>My Recent Work</h5>
+            <h2>Portfolio</h2>
+            <div className='container portfolio'>
+                <article className='portfolio__item'>
+                    <div className='portfolio__item-image'>
+                        <img src={projectImg1} alt="Bookings App" />
+                    </div>
+                    <h3>This is a portfolio item title</h3>
+                    <a href='https://github.com/Agrossio/bookings' className='btn' target={'_blank'} rel="noreferrer">Github</a>
+                    <a href='https://cruce-bookingapp.vercel.app/' className='btn btn-primary' target={'_blank'} rel="noreferrer">Live Demo</a>
+                </article>
+            </div>
+        </section>
     )
 }
 
