432d42113be81a09d3d64a7d0a7a46473c1b05ff
Agrossio <abossio@gmail.com>
2022-10-26 17:52:14 -0300

    Portfolio OK

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,51 @@
 import React from 'react'
 import './portfolio.css'
-import projectImg1 from '../../assets/projects/project1.png'
+import img1 from '../../assets/projects/project1.png'
+
+const projectsArray = [
+    {
+        id: 1,
+        image: img1,
+        title: 'Bookings App',
+        github: 'https://github.com/Agrossio/bookings',
+        demo: 'https://cruce-bookingapp.vercel.app/',
+    },
+    {
+        id: 2,
+        image: img1,
+        title: 'Bookings App',
+        github: 'https://github.com/Agrossio/bookings',
+        demo: 'https://cruce-bookingapp.vercel.app/',
+    },
+    {
+        id: 3,
+        image: img1,
+        title: 'Bookings App',
+        github: 'https://github.com/Agrossio/bookings',
+        demo: 'https://cruce-bookingapp.vercel.app/',
+    },
+    {
+        id: 4,
+        image: img1,
+        title: 'Bookings App',
+        github: 'https://github.com/Agrossio/bookings',
+        demo: 'https://cruce-bookingapp.vercel.app/',
+    },
+    {
+        id: 5,
+        image: img1,
+        title: 'Bookings App',
+        github: 'https://github.com/Agrossio/bookings',
+        demo: 'https://cruce-bookingapp.vercel.app/',
+    },
+    {
+        id: 6,
+        image: img1,
+        title: 'Bookings App',
+        github: 'https://github.com/Agrossio/bookings',
+        demo: 'https://cruce-bookingapp.vercel.app/',
+    },
+]
 
 const Portfolio = () => {
     return (
@@ -9,76 +54,22 @@ const Portfolio = () => {
             <h2>Portfolio</h2>
 
             <div className='container portfolio__container'>
-                <article className='portfolio__item'>
-                    <div className='portfolio__item-image'>
-                        <img src={projectImg1} alt="Bookings App" />
-                    </div>
-                    <h3>This is a portfolio item title</h3>
-                    <div className='portfolio__item-cta'>
-                        <a href='https://github.com/Agrossio/bookings' className='btn' target={'_blank'} rel="noreferrer">Github</a>
-                        <a href='https://cruce-bookingapp.vercel.app/' className='btn btn-primary' target={'_blank'} rel="noreferrer">Live Demo</a>
-                    </div>
-                </article>
-                <article className='portfolio__item'>
-                    <div className='portfolio__item-image'>
-                        <img src={projectImg1} alt="Bookings App" />
-                    </div>
-                    <h3>This is a portfolio item title</h3>
-                    <div className='portfolio__item-cta'>
-                        <a href='https://github.com/Agrossio/bookings' className='btn' target={'_blank'} rel="noreferrer">Github</a>
-                        <a href='https://cruce-bookingapp.vercel.app/' className='btn btn-primary' target={'_blank'} rel="noreferrer">Live Demo</a>
-                    </div>
-                </article>
-                <article className='portfolio__item'>
-                    <div className='portfolio__item-image'>
-                        <img src={projectImg1} alt="Bookings App" />
-                    </div>
-                    <h3>This is a portfolio item title</h3>
-                    <div className='portfolio__item-cta'>
-                        <a href='https://github.com/Agrossio/bookings' className='btn' target={'_blank'} rel="noreferrer">Github</a>
-                        <a href='https://cruce-bookingapp.vercel.app/' className='btn btn-primary' target={'_blank'} rel="noreferrer">Live Demo</a>
-                    </div>
-                </article>
-                <article className='portfolio__item'>
-                    <div className='portfolio__item-image'>
-                        <img src={projectImg1} alt="Bookings App" />
-                    </div>
-                    <h3>This is a portfolio item title</h3>
-                    <div className='portfolio__item-cta'>
-                        <a href='https://github.com/Agrossio/bookings' className='btn' target={'_blank'} rel="noreferrer">Github</a>
-                        <a href='https://cruce-bookingapp.vercel.app/' className='btn btn-primary' target={'_blank'} rel="noreferrer">Live Demo</a>
-                    </div>
-                </article>
-                <article className='portfolio__item'>
-                    <div className='portfolio__item-image'>
-                        <img src={projectImg1} alt="Bookings App" />
-                    </div>
-                    <h3>This is a portfolio item title</h3>
-                    <div className='portfolio__item-cta'>
-                        <a href='https://github.com/Agrossio/bookings' className='btn' target={'_blank'} rel="noreferrer">Github</a>
-                        <a href='https://cruce-bookingapp.vercel.app/' className='btn btn-primary' target={'_blank'} rel="noreferrer">Live Demo</a>
-                    </div>
-                </article>
-                <article className='portfolio__item'>
-                    <div className='portfolio__item-image'>
-                        <img src={projectImg1} alt="Bookings App" />
-                    </div>
-                    <h3>This is a portfolio item title</h3>
-                    <div className='portfolio__item-cta'>
-                        <a href='https://github.com/Agrossio/bookings' className='btn' target={'_blank'} rel="noreferrer">Github</a>
-                        <a href='https://cruce-bookingapp.vercel.app/' className='btn btn-primary' target={'_blank'} rel="noreferrer">Live Demo</a>
-                    </div>
-                </article>
-                <article className='portfolio__item'>
-                    <div className='portfolio__item-image'>
-                        <img src={projectImg1} alt="Bookings App" />
-                    </div>
-                    <h3>This is a portfolio item title</h3>
-                    <div className='portfolio__item-cta'>
-                        <a href='https://github.com/Agrossio/bookings' className='btn' target={'_blank'} rel="noreferrer">Github</a>
-                        <a href='https://cruce-bookingapp.vercel.app/' className='btn btn-primary' target={'_blank'} rel="noreferrer">Live Demo</a>
-                    </div>
-                </article>
+                {
+                    projectsArray.map((project, id) => {
+                        return (
+                            <article key={id} className='portfolio__item'>
+                                <div className='portfolio__item-image'>
+                                    <img src={project.image} alt={project.title} />
+                                </div>
+                                <h3>{project.title}</h3>
+                                <div className='portfolio__item-cta'>
+                                    <a href={project.github} className='btn' target={'_blank'} rel="noreferrer">Github</a>
+                                    <a href={project.demo} className='btn btn-primary' target={'_blank'} rel="noreferrer">Live Demo</a>
+                                </div>
+                            </article>
+                        )
+                    })
+                }
             </div>
         </section>
     )
