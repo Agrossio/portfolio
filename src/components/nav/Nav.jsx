97abcf1d38e266d3bd2e821f771bea8f2b5ff93d
Agrossio <abossio@gmail.com>
2022-11-01 01:41:52 -0300

    Socials & Scroll Down 1.0

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -7,6 +7,8 @@ import { BsPatchCheck, BsStackOverflow, BsStack } from 'react-icons/bs'
 import { RiStarLine } from 'react-icons/ri'
 import { BiMessageSquareDetail } from 'react-icons/bi'
 import { useState } from 'react'
+import underConstruction from '../../assets/images/under-construction/under-construction-transparent.png'
+import HeaderSocials from '../header/HeaderSocials'
 
 
 const Nav = () => {
@@ -14,13 +16,18 @@ const Nav = () => {
     const [activeNav, setActiveNav] = useState('#');
 
     return (
-        <nav>
-            <a href='#header' className={activeNav === '#' ? 'active' : ''} onClick={() => setActiveNav('#')}><GiMushroomHouse /></a>
-            <a href='#about' className={activeNav === '#about' ? 'active' : ''} onClick={() => setActiveNav('#about')}><AiOutlineUser /></a>
-            <a href='#stack' className={activeNav === '#stack' ? 'active' : ''} onClick={() => setActiveNav('#stack')}><BsStack /></a>
-            <a href='#featured' className={activeNav === '#featured' ? 'active' : ''} onClick={() => setActiveNav('#featured')}><RiStarLine /></a>
-            <a href='#contact' className={activeNav === '#contact' ? 'active' : ''} onClick={() => setActiveNav('#contact')}><BiMessageSquareDetail /></a>
-        </nav >
+        <>
+            <img src={underConstruction} className='under__construction' alt='Under Construction Sign' />
+            <HeaderSocials />
+            <nav>
+                <a href='#header' className={activeNav === '#' ? 'active' : ''} onClick={() => setActiveNav('#')}><GiMushroomHouse /></a>
+                <a href='#about' className={activeNav === '#about' ? 'active' : ''} onClick={() => setActiveNav('#about')}><AiOutlineUser /></a>
+                <a href='#stack' className={activeNav === '#stack' ? 'active' : ''} onClick={() => setActiveNav('#stack')}><BsStack /></a>
+                <a href='#featured' className={activeNav === '#featured' ? 'active' : ''} onClick={() => setActiveNav('#featured')}><RiStarLine /></a>
+                <a href='#contact' className={activeNav === '#contact' ? 'active' : ''} onClick={() => setActiveNav('#contact')}><BiMessageSquareDetail /></a>
+            </nav >
+            {/* <a href='#contact' className='scroll_down'>Scroll Down</a> */}
+        </>
     )
 }
 
