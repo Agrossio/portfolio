e5bba27d2f0e17b0a42d31685ff24682ddd2d5ed
Agrossio <abossio@gmail.com>
2023-08-04 12:39:50 -0300

    content updated 04-08-23

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import './about.css'
 import ME from '../../assets/images/profile1.png'
 import { FaAward } from 'react-icons/fa'
-import { VscFolderLibrary } from 'react-icons/vsc'
-import { SiPepsi } from 'react-icons/si'
+// import { VscFolderLibrary } from 'react-icons/vsc'
+import { SiPepsi, SiMicrosoftedge } from 'react-icons/si'
 
 const About = () => {
     return (
@@ -33,15 +33,15 @@ const About = () => {
                                 <small>Worked 9+ Years</small>
                             </article>
                         </a>
-                        <a href="#services">
+                        <a href="https://asjservicios.com.ar/" target={'_blank'} rel="noreferrer">
                             <article className='about__card'>
-                                <VscFolderLibrary className='about__icon' />
-                                <h5>Projects</h5>
-                                <small>10+ Completed</small>
+                                <SiMicrosoftedge className='about__icon' />
+                                <h5>ASJ Sevicios</h5>
+                                <small>Angular + Java Dev</small>
                             </article>
                         </a>
                     </div>
-                    <p>My name is Matias Abossio, I'm a Jr Full Stack Developer from Argentina and also a 9 year Consumer Goods Industry experienced Engineer. I've graduated from Plataforma 5 Coding Bootcamp, adding +800 hours of coding applying Pair Programming and Agile Methodologies to my background.
+                    <p>My name is Matias Abossio, I'm a Jr Full Stack Developer from Argentina and also a 9 year Consumer Goods Industry experienced Engineer. Nowadays, I'm working at ASJ Servicios (a Grupo Petersen Company), after finishing an in company bootcamp. On October-22 I've graduated from Plataforma 5 Coding Bootcamp, adding +800 hours of coding applying Pair Programming and Agile Methodologies to my background.
                     </p>
 
                     <a href='#contact' className='btn btn-primary'>Let's Talk</a>
